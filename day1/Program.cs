8bd611131c65a2738139a943b30cf7a1b5126a49
RademakerJ <j.rademaker@live.nl>
2020-12-02 21:56:24 +0100

    Added day 1 and day 2

---
+++ day1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get file
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var filePath = Path.Combine(projectDirectory, "data", "data.txt");
+
+            // Fill expenses
+            var expenses = new List<int>();
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            while (!streamReader.EndOfStream)
+            {
+                if (int.TryParse(streamReader.ReadLine(), out int x))
+                {
+                    expenses.Add(x);
+                }
+            }
+
+            // Find two numbers that sum to 2020
+            int[,,] expensesSum = new int[expenses.Count, expenses.Count, expenses.Count];
+            bool found = false;
+
+            for (int i = 0; i < expensesSum.GetUpperBound(0); i++)
+            {
+                if (found) { break; }
+                for (int j = 0; j < expensesSum.GetUpperBound(1); j++)
+                {
+                    if (found) { break; }
+                    for (int k = 0; k < expensesSum.GetUpperBound(2); k++)
+                    {
+                        if ((expenses[i] + expenses[j] + expenses[k]) == 2020)
+                        {
+                            Console.WriteLine(expenses[i] * expenses[j] * expenses[k]);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            /*
+            // "Lookup table" for fun
+            (int index1, int index2, int index3) = FindIndicesForValue(expensesSum, 2020);
+
+            // Quick check
+            if (index1 >= 0)
+            {
+                Console.WriteLine($"Value 1: {expenses[index1]}, Value 2: {expenses[index2]}, Value 3: {expenses[index3]}");
+                Console.WriteLine($"Multiplication: {expenses[index1] * expenses[index2] * expenses[index3]}");
+            }
+
+            // Find the fist occurrence of the value
+            // Return -1, -1, -1 if nothing was found
+            static (int, int, int) FindIndicesForValue(int[,,] array, int value)
+            {
+                for (int i = 0; i < array.GetUpperBound(0); i++)
+                {
+                    for (int j = 0; j < array.GetUpperBound(1); j++)
+                    {
+                        for (int k = 0; k < array.GetUpperBound(2); k++)
+                        {
+                            if (array[i, j, k] == value)
+                            {
+                                return (i, j, k);
+                            }
+                        }
+                    }
+                }
+                return (-1, -1, -1);
+            }
+            */
+        }
+    }
+}
---
+++ day2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get file
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var filePath = Path.Combine(projectDirectory, "data", "data.txt");
+
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            int minMaxCount = 0;
+            int indicesCount = 0;
+            while (!streamReader.EndOfStream)
+            {
+                var splitElements = streamReader.ReadLine().Split(" ");
+                (int left, int right) = GetLeftRight(splitElements[0]);
+                var character = char.Parse(splitElements[1].Replace(":", string.Empty));
+                var password = splitElements[2];
+
+                // Method 1: minmax, Method 2: indices (only 1 must contain the character)
+                minMaxCount += IsValidMinMaxPassword(password, character, left, right) ? 1 : 0;
+                indicesCount += IsValidIndicesPassword(password, character, left, right) ? 1 : 0;
+
+            }
+
+            // Log the outcomes
+            Console.WriteLine($"Min-max password count:\n{minMaxCount}");
+            Console.WriteLine($"Indices password count(only 1 must contain the character):\n{indicesCount}");
+
+            // Check the indices. The string must only contain the character at one of the indices.
+            static bool IsValidIndicesPassword(string password, char character, int firstIndex, int secondIndex)
+            {
+                return StringContainsElementAt(password, character, firstIndex) ^ StringContainsElementAt(password, character, secondIndex);
+            }
+
+            // Check min-max. The amount of characters (that equal character) must be between the min-max.
+            static bool IsValidMinMaxPassword(string password, char character, int min, int max)
+            {
+                var characterCount = password.Count(c => c == character);
+                return characterCount >= min && characterCount <= max;
+            }
+
+            // Format string = 5-7. Remove dash and parse element at first and second index.
+            static (int, int) GetLeftRight(string leftRight)
+            {
+                var leftRightInts = leftRight.Split("-");
+
+                return (int.Parse(leftRightInts[0]), int.Parse(leftRightInts[1]));
+            }
+
+            // Index is not zero based (starts at 1). This function subtracts 1 from index.
+            static bool StringContainsElementAt(string input, char character, int index)
+            {
+                if (index - 1 < 0)
+                {
+                    return false;
+                }
+
+                return input.ElementAt(index - 1).Equals(character);
+            }
+
+        }
+    }
+}
