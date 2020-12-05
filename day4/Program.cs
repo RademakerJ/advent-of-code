07297083108de6b00a9d6281fb14cb36082a9340
RademakerJ <j.rademaker@live.nl>
2020-12-05 22:27:46 +0100

    Added day 4 and day 5

---
+++ day4/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get file
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var filePath = Path.Combine(projectDirectory, "data", "data.txt");
+
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            //Console.WriteLine($"Passports with required fields: {GetPassportsWithRequiredFields(streamReader).Count}"); // Problem 1
+            Console.WriteLine($"Valid passports: {GetValidPassports(streamReader).Count}"); // Problem 2
+
+
+            static List<Passport> GetValidPassports(StreamReader streamReader)
+            {
+                var passports = GetPassportsWithRequiredFields(streamReader);
+                Console.WriteLine(passports.Count);
+                var passedPassports = new List<Passport>();
+
+                // I should go to hell once again
+                for (int i = 0; i < passports.Count; i++)
+                {
+                    bool invalid = false;
+                    foreach (var (key, value) in passports[i].Fields)
+                    {
+                        if (key.Equals("eyr"))
+                        {
+                            if (!ValidateNumber(value, 2020, 2030)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("iyr"))
+                        {
+                            if (!ValidateNumber(value, 2010, 2020)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("byr"))
+                        {
+                            if (!ValidateNumber(value, 1920, 2002)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("hgt"))
+                        {
+                            if (!ValidateHeight(value)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("ecl"))
+                        {
+                            if (!ValidateEyeColor(value)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("hcl"))
+                        {
+                            if (!ValidateHairColor(value)) { invalid = true; break; };
+                        }
+                        else if (key.Equals("pid"))
+                        {
+                            if (!ValidatePassportId(value)) { invalid = true; break; };
+                        }
+                    }
+
+                    if (!invalid)
+                    {
+                        passedPassports.Add(passports[i]);
+                    }
+                }
+                return passedPassports;
+            }
+
+            static List<Passport> GetPassportsWithRequiredFields(StreamReader streamReader)
+            {
+                var passportsRaw = streamReader.ReadToEnd().Split(new string[] { Environment.NewLine + Environment.NewLine },
+                               StringSplitOptions.RemoveEmptyEntries);
+
+                var validPassports = new List<Passport>();
+                var requiredFields = new List<string> { "eyr", "hgt", "iyr", "ecl", "byr", "hcl", "pid" };
+                for (int i = 0; i < passportsRaw.Length; i++)
+                {
+                    var components = passportsRaw[i]
+                        .Replace(Environment.NewLine, " ")
+                        .Split(new char[] { ' ', ':' });
+                    var fields = components.Where((x, i) => i % 2 == 0);
+                    var values = components.Where((x, i) => i % 2 != 0);
+
+                    if (requiredFields.All(value => fields.Contains(value)))
+                    {
+                        var dict = fields.Zip(values).ToDictionary(x => x.First, x => x.Second);
+                        validPassports.Add(new Passport(dict));
+                    }
+                }
+
+                return validPassports;
+            }
+
+            static bool ValidateNumber(string value, int min, int max)
+            {
+                // I give up.
+                return int.TryParse(value, out int number) && number >= min && number <= max;
+            }
+
+            static bool ValidateHeight(string value)
+            {
+                return Regex.Match(value, "^(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in$").Success;
+            }
+
+            static bool ValidateHairColor(string value)
+            {
+                return Regex.Match(value, "^#[0-9a-f]{6}$").Success;
+            }
+
+            static bool ValidateEyeColor(string value)
+            {
+                return Regex.Match(value, @"^(amb|blu|brn|gry|grn|hzl|oth)\b").Success;
+            }
+
+            static bool ValidatePassportId(string value)
+            {
+                return Regex.Match(value, "^[0-9]{9}$").Success;
+            }
+        }
+
+        struct Passport
+        {
+            public Dictionary<string, string> Fields { get; }
+
+            public Passport(Dictionary<string, string> fields)
+            {
+                Fields = fields;
+            }
+        }
+    }
+}
---
+++ day5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get file
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var filePath = Path.Combine(projectDirectory, "data", "data.txt");
+
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            int highestSeatId = 0;
+            var seatIds = new List<int>();
+
+            while (!streamReader.EndOfStream)
+            {
+                (int min, int max) rows = (0, 127);
+                (int min, int max) columns = (0, 7);
+                var seat = streamReader.ReadLine();
+
+                for (int i = 0; i < seat.Length; i++)
+                {
+                    if (seat[i].Equals('F') || seat[i].Equals('B'))
+                    {
+                        rows = GetSplit(seat[i], rows.min, rows.max);
+                    }
+                    else if (seat[i].Equals('L') || seat[i].Equals('R'))
+                    {
+                        columns = GetSplit(seat[i], columns.min, columns.max);
+                    }
+                }
+
+                var seatId = (rows.min * 8) + columns.min;
+                seatIds.Add(seatId);
+                highestSeatId = seatId > highestSeatId ? seatId : highestSeatId;
+            }
+
+            seatIds = seatIds.OrderBy(x => x).ToList();
+            var mySeatId = Enumerable.Range(seatIds[0], seatIds[seatIds.Count - 1]).Except(seatIds).First();
+
+            Console.WriteLine("Highest boarding pass ID: " + highestSeatId);
+            Console.WriteLine("My seat ID: " + mySeatId);
+
+            static (int, int) GetSplit(char input, int min, int max)
+            {
+                if (input.Equals('F') || input.Equals('L'))
+                {
+                    return (min, (min + max) / 2);
+                }
+
+                if (input.Equals('B') || input.Equals('R'))
+                {
+                    return ((min + max) / 2 + 1, max);
+                }
+
+                return (min, max);
+            }
+        }
+    }
+}
