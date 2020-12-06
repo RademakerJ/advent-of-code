83d99ca3be857e0d1f1483d58609dc5714c39901
RademakerJ <j.rademaker@live.nl>
2020-12-06 13:36:59 +0100

    Added day 6

---
+++ day6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get file
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var filePath = Path.Combine(projectDirectory, "data", "data.txt");
+
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            var groupAnswers = streamReader.ReadToEnd().Split(new string[] { Environment.NewLine + Environment.NewLine },
+                               StringSplitOptions.RemoveEmptyEntries);
+
+            var anyoneCount = groupAnswers.Select(groupAnswer => groupAnswer.Replace(Environment.NewLine, string.Empty).ToCharArray().Distinct().Count()).Sum();
+            Console.WriteLine("Sum of yes (any): " + anyoneCount); // Problem 1
+
+            var everyoneCount = groupAnswers.Select(groupAnswer => groupAnswer.Replace(Environment.NewLine, string.Empty).GroupBy(eYes => eYes)
+                .Count(group => group.Count() == groupAnswer.Count(c => c.Equals('\n')) + 1)).Sum();
+            Console.WriteLine("Sum of yes (every): " + everyoneCount); // Problem 2
+        }
+    }
+}
